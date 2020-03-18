2fa6490ad9ba19434a3e649e6cf4ddbe98c03e7d
62random <josepedropinto98@gmail.com>
2020-03-18 00:24:15 +0000

    render

--- grid_world.py
+++ grid_world.py
@@ -1,5 +1,8 @@
+from PIL import Image, ImageDraw, ImageColor
+from IPython.display import display
+
 class GridWorld:
-    def __init__(self, rows, cols, win, lose_states, obstacles, start, deterministic):
+    def __init__(self, rows, cols, win, lose_states, obstacles, start):
         # Variáveis Globais 
         self.BOARD_ROWS = rows
         self.BOARD_COLS = cols
@@ -7,7 +10,6 @@ class GridWorld:
         self.LOSE_STATES = lose_states
         self.OBSTACLES = obstacles
         self.START = start
-        self.DETERMINISTIC = deterministic
         self.action_space = ['up', 'down', 'left', 'right']
 
         self.state = self.START
@@ -62,3 +64,38 @@ class GridWorld:
         self.state = self.START
         self.steps = 0
         self.step_reward = 0
+
+    def render(self):
+        width = 600
+        cell_width = int(width/self.BOARD_COLS)
+        cell_height = int(cell_width)
+        height = cell_height * self.BOARD_ROWS
+        
+
+        image = Image.new(mode='RGB', size=(width, height), color='white')
+
+        draw = ImageDraw.Draw(image)
+        for i in range(self.BOARD_COLS - 1):
+            vertical_line = ((i*cell_width + cell_width, 0),(i*cell_width + cell_width, image.height))
+            draw.line(vertical_line, fill='black')
+
+        for i in range(self.BOARD_ROWS - 1):
+            horizontal_line = ((0, i*cell_height + cell_height),(image.width, i*cell_height + cell_height))
+            draw.line(horizontal_line, fill='black')
+
+        convert_pos = lambda a: (a[1]*cell_width, a[0]*cell_height)
+        square = lambda a: [(a[0], a[1]), (a[0] + cell_width, a[1] + cell_height)]
+
+        for p in self.OBSTACLES:
+            draw.rectangle(square(convert_pos(p)), fill='black')
+        
+        for p in self.LOSE_STATES:
+            draw.rectangle(square(convert_pos(p)), fill='red')
+        
+        draw.rectangle(square(convert_pos(self.WIN_STATE)), fill='green')
+
+        draw.rectangle(square(convert_pos(self.START)), fill='yellow')
+        
+        del draw
+        return display(image)
+
